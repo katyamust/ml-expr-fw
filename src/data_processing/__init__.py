7bfac95bb5ce5e6a215413f399231c2d0cc5d6eb
katyamust <kamustaf@microsoft.com>
2020-05-13 11:41:07 +0200

    fix build

--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -1,5 +1,4 @@
 from .data_processor import DataProcessor
 from .empty_processor import EmptyProcessor
-from src.data_processing.text.text_processor import TextProcessor
 
-__all__ = ["DataProcessor", "EmptyProcessor", "TextProcessor", "NltkTextProcessor", "SpacyTextProcessor"]
+__all__ = ["DataProcessor", "EmptyProcessor"]
