e1985823a8605cf0b50574bd417541c8efcdca01
katyamust <kamustaf@microsoft.com>
2020-05-13 13:29:20 +0200

    remove __str__ and __repr__ from data processor

--- src/data_processing/data_processor.py
+++ src/data_processing/data_processor.py
@@ -17,11 +17,3 @@ class DataProcessor(ABC):
     @abstractmethod
     def apply_batch (self, **kwargs):
         pass
-
-    @abstractmethod
-    def __str__(self):
-        pass
-
-    @abstractmethod
-    def __repr__(self):
-        pass
