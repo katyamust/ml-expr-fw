8124847aa6974cfb019a076c766d4ad13be61ec7
katyamust <kamustaf@microsoft.com>
2020-05-16 22:50:45 +0200

    fix depedencies

--- mlfabric/__init__.py
+++ mlfabric/__init__.py
@@ -1,6 +1,7 @@
 from .evaluation import EvaluationResult, Evaluator, TimeTook
 from .experiment_runner import ExperimentRunner
+from .models import BaseModel
 from .data_processing import EmptyProcessor
 from mlfabric.data_processing.text import SpacyTextProcessor
 
-__all__ = ['ExperimentRunner', 'EvaluationResult', 'Evaluator', 'TimeTook', "EmptyProcessor","SpacyTextProcessor"]
+__all__ = ['ExperimentRunner', 'EvaluationResult', 'Evaluator', 'TimeTook', "BaseModel",  "EmptyProcessor","SpacyTextProcessor"]
