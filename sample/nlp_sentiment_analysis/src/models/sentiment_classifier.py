1267626f2276f73288f23005955c6eae685abe6e
katyamust <kamustaf@microsoft.com>
2020-05-27 22:15:37 +0200

    with model and unit tests

---
+++ sample/nlp_sentiment_analysis/src/models/sentiment_classifier.py
@@ -0,0 +1,62 @@
+import logging
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+
+from src.models import BaseModel
+from src.data_processing import EmptyProcessor
+
+
+class SentimentClassifier(BaseModel):
+    """
+    Example model implementation to demonstrate the approach
+    Model that predicts the movie review text sentiment: is it positive or negative review.
+    """
+
+    def __init__(self, model_name='SentimentClassifier',
+                 preprocessor=EmptyProcessor(),
+                 postprocessor=EmptyProcessor()
+                 ):
+        """
+        :param model_name: name of model
+        :param preprocessor: TextPreprocessor object for text data in this example
+        :param postprocessor: TextPostprocessor object for text data in this example
+        """
+
+        self.vectorizer = TfidfVectorizer()
+        self.clf = SGDClassifier()
+
+        super().__init__(model_name=model_name,
+                         preprocessor=preprocessor,
+                         postprocessor=postprocessor)
+
+    def fit(self, X, y=None) -> None:
+        """
+        Fits model
+        Calculates TF-IDF vectors and fits SGDClassifier model
+        :param X: input train data list of movie reviews
+        :param y: input train label sentiment for each review
+        """
+        corpus = self.preprocessor.apply(X)
+
+        logging.info("Finished preprocessing input data, fitting TF IDF vectorizer")
+
+        X_tf_idf = self.vectorizer.fit_transform(corpus)
+
+        logging.info("Fitting classifier")
+        self.clf.fit(X_tf_idf, y)
+
+        logging.info("Finished fitting model")
+
+    def predict(self, X) -> pd.DataFrame:
+        """
+        Predicts movie reviews  sentiment
+        :param X: list of of movie text reviews
+        :return:  list of sentiment value 0 or 1 for each review
+        """
+        corpus = self.preprocessor.apply(X)
+        Xp_tf_idf = self.vectorizer.transform(corpus)
+        y_predicted = self.clf.predict(Xp_tf_idf)
+        self.postprocessor.apply(y_predicted)
+        return y_predicted
---
+++ sample/nlp_sentiment_analysis/tests/test_sentiment_classifier.py
@@ -0,0 +1,44 @@
+import pytest
+
+import pandas as pd
+
+from src.models.sentiment_classifier import SentimentClassifier
+
+@pytest.fixture
+def mock_train_df():
+    return pd.DataFrame(
+        {
+            "text": ["excellent movie", "did not like it", "horrible actors"],
+            "label": [1, 0, 0],
+        },
+        columns=["text", "label"],
+    )
+
+
+@pytest.fixture
+def mock_test_df():
+    return pd.DataFrame(
+        {"text": ["What I can say about this movie", "ha ha ha"]}, columns=["text"]
+    )
+
+
+def test_sentiment_classifier_can_fit(mock_train_df):
+
+    model = SentimentClassifier()
+    X = mock_train_df["text"].values
+    y = mock_train_df["label"].values
+    model.fit(X, y)
+
+    assert pytest.approx(model.clf.coef_[0][0], 0.001) == -6.93
+
+
+def test_sentiment_classifier_predict(mock_train_df, mock_test_df):
+    model = SentimentClassifier()
+    X = mock_train_df["text"].values
+    y = mock_train_df["label"].values
+    model.fit(X, y)
+    Xp = mock_test_df["text"].values
+    y = model.predict(Xp)
+
+    assert y[0] == 1
+
